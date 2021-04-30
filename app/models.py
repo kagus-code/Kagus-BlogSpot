2bfb376bb19653ad7b95b5d4437c020ea7ba54a6
Eston Kagwima <ekagwima745@gmail.com>
2021-04-30 23:05:50 +0300

    adding Role model and its user relationship

--- app/models.py
+++ app/models.py
@@ -10,10 +10,23 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 
     
