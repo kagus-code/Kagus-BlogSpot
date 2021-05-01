cad5f65f4e569fda405d81a4d37db6c06033f9f6
Eston Kagwima <ekagwima745@gmail.com>
2021-05-01 20:03:22 +0300

    configuring upload photo functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 
@@ -14,6 +15,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 fa = FontAwesome()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -26,6 +28,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     fa.init_app(app)
+    configure_uploads(app,photos)
 
     # blueprints
     from .main import main as main_blueprint
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = '6a5b826c18f5db76e78adf63ecd0af76'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
 class ProdConfig(Config):
     pass
