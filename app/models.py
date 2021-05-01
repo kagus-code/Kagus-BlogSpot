44d79adddc5b775234c9b4d69a5b38addb0c8861
Eston Kagwima <ekagwima745@gmail.com>
2021-05-01 12:07:15 +0300

    adding flask login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from . import db 
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
 
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -29,9 +31,9 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.pass_secure, password)
 
-    # @login_manager.user_loader
-    # def load_user(user_id):
-    #     return User.query.get(int(user_id))    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))    
 
     def __repr__(self):
         return f'User {self.username}'
