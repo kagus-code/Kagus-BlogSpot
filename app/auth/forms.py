97453474d8ba0187ecedb0160d7c9f466f8813b6
Eston Kagwima <ekagwima745@gmail.com>
2021-05-01 13:17:38 +0300

    adding registration form and routes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,23 @@
 from flask_wtf import FlaskForm
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError,SelectField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(),  Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    role= SelectField('Role', choices=[('writer','writer'),('reader','reader')],validators=[Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
 
    
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,30 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,logout_user,login_required
+from ..models import User
 from .forms import RegistrationForm
+from .. import db
 from . import auth
 
 
 
-@auth.route('/register', methods = ['GET', 'POST'])
+
+@auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
-    return render_template('auth/register.html', title = 'Register', form = form)
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data,role=form.role.data)
+        db.session.add(user)
+        db.session.commit()
+
+
+  #      mail_message("Welcome to Pitch-Piper","email/welcome_user",user.email,user=user)
+
+        
+
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 
 @auth.route('/login')
