94230b78a76011b211e82f2a21570c3f770cecb1
Eston Kagwima <ekagwima745@gmail.com>
2021-05-03 19:49:01 +0300

    fix indentation error on init.py

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,9 @@ def create_app(config_name):
     simple.init_app(app)
 
     with app.app_context():
-    db.create_all()
+        
+
+        db.create_all()
 
     # blueprints
     from .main import main as main_blueprint
