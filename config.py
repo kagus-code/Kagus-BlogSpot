a48e5bbd044a9c1b8b87927ef6007e54ed0054fa
Eston Kagwima <ekagwima745@gmail.com>
2021-05-05 09:12:35 +0300

    updating heroku

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
 
 class ProdConfig(Config):
 
- #   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace("://", "ql://", 1)
 
 
 class TestConfig(Config):
